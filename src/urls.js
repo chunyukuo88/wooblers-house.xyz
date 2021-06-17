832329d9e6aff2a11d7fbf87d4a11c54697ced61
Alex Gochenour <alexgochenour@gmail.com>
2021-06-16 22:08:37 -0400

    Moved variables to .env file.

--- src/urls.js
+++ src/urls.js
@@ -1,11 +1,9 @@
-const openWeatherApiKey = 'eb366c82727f387afc53658766e245e8';
-
 const urls = {
-    captions: `https://woobler-photos.s3.amazonaws.com/captions.txt`,
-    githubRepo: 'https://github.com/chunyukuo88/Brothaus',
-    openWeatherUrl: 'https://api.openweathermap.org/data/2.5/weather?q=Columbus,Ohio&appid=' + openWeatherApiKey,
+    captions: process.env.REACT_APP_CAPTIONS,
+    githubRepo: process.env.REACT_APP_GITHUB_REPO,
+    openWeatherUrl: `https://api.openweathermap.org/data/2.5/weather?q=Columbus,Ohio&appid=${process.env.REACT_APP_WEATHER_API_KEY}`,
 };
 
-export const getFotoSource = (integer) => `https://woobler-photos.s3.amazonaws.com/${integer} (Custom).JPG`;
+export const getFotoSource = (integer) => `${process.env.REACT_APP_FOTO_SOURCE}/${integer} (Custom).JPG`;
 
 export default urls;
