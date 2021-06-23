f5e20039de1aa7565ab8422212b1addde41d0f21
Alex Gochenour <alexgochenour@gmail.com>
2021-06-23 18:54:01 -0400

    First passing test after figuring out how to mock the environment variable.

--- src/components/Carousel/Display.js
+++ src/components/Carousel/Display.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import staticStrings from '../../StaticStrings';
-import { useSelector } from 'react-redux';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
 export const Display = ({ photosObject }) => {
-  if (!photosObject) return <ErrorMessage />;
-  return photosObject?.Contents?.map((obj, key) => {
-    const numberedSrc = `${process.env.REACT_APP_FOTO_SOURCE}/${key + 1} (Custom).JPG`;
-    return (
-      <div className='photo' data-testid='photo' key={key} >
-        <img src={numberedSrc} alt='test'/>
-      </div>
-    );
-  });
-}
+  const [currentPhoto, setCurrent] = React.useState(0);
+  const photoArray = photosObject?.Contents;
 
-const ErrorMessage = () => {
-  const language = useSelector((state) => state.language);
+  const nextSlide = () => {
+    setCurrent(
+      currentPhoto === photoArray.length - 1
+        ? 0
+        : currentPhoto + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setCurrent(
+      currentPhoto === 0
+        ? photoArray.length - 1
+        : currentPhoto - 1
+    );
+  };
+
   return (
-    <div className={language} data-testid='error-message'>
-      {staticStrings.errorLoadingPhotos[language]}
+    <div>
+      <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
+      <DisplayedPhoto photo={currentPhoto}/>
+      <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
     </div>
+  )
+};
+
+const DisplayedPhoto = (photoObject) => {
+  const photoUrl = getPhotoUrl(photoObject.photo);
+  return (
+    <img className={photoUrl} data-testid='photo' key={photoObject} src={photoUrl} alt='test'/>
   );
 };
+
+const getPhotoUrl = (key) => `${process.env.REACT_APP_FOTO_SOURCE}/${key + 1} (Custom).JPG`;
+
+// const ErrorMessage = () => {
+//   const language = useSelector((state) => state.language);
+//   return (
+//     <div className={language} data-testid='error-message'>
+//       {staticStrings.errorLoadingPhotos[language]}
+//     </div>
+//   );
+// };
--- src/components/Carousel/Display.test.js
+++ src/components/Carousel/Display.test.js
@@ -1,6 +1,5 @@
 import { Display } from './Display';
 import { render, screen } from '@testing-library/react';
-import Root from '../../Root';
 import staticStrings from '../../StaticStrings';
 
 const { errorLoadingPhotos } = staticStrings;
@@ -8,27 +7,63 @@ const initialState = { language: 'english', };
 
 describe('Display()', ()=>{
   describe('GIVEN: The component has loaded,', ()=>{
-    describe('AND: An error prevents the photos from loading,', ()=>{
-      it('THEN: An error message displays instead of the photos.', ()=>{
-        render(
-          <Root initialState={initialState}>
-            <Display />
-          </Root>
-        );
-        const errorMessage = screen.getByTestId('error-message');
-        expect(errorMessage).toBeDefined();
-        expect(errorMessage).toHaveTextContent(errorLoadingPhotos.english);
-      });
-      it('THEN: Localized error messages display properly.', ()=>{
-        initialState.language = 'chinese';
-        render(
-          <Root initialState={initialState}>
-            <Display />
-          </Root>
-        );
-        const errorMessage = screen.getByTestId('error-message');
-        expect(errorMessage).toHaveTextContent(errorLoadingPhotos.chinese);
-      });
+    it('THEN: The first photograph is displayed.', ()=>{
+      const photosObject = {
+        Contents: [
+          { photo: 0 },
+          { photo: 1 },
+          { photo: 2 },
+        ],
+      };
+      render(<Display {...photosObject}/>);
+      const displayedImage = screen.getByTestId('photo');
+
+      expect(displayedImage).toHaveClass('www.mock-photo-source.com/1 (Custom).JPG');
     });
   });
+  describe('WHEN: The user clicks the right arrow', ()=>{
+    it('THEN: The next photograph is revealed.', ()=>{
+      // const photosObject = {
+      //   Contents: [],
+      // };
+      // const { container } = render(<Display {...photosObject}/>);
+    });
+  });
+  describe('WHEN: The user clicks the left arrow', ()=>{
+    it('THEN: The previous photo is revealed.', ()=>{
+      //
+    });
+  });
+  describe('WHEN: The user clicks the left arrow while viewing the first photo', ()=>{
+    it('THEN: It remains at the first photo.', ()=>{
+      //
+    });
+  });
+  describe('WHEN: The user clicks the right arrow and there are no more photos', ()=>{
+    it('THEN: It cycles back to the first photo.', ()=>{
+      //
+    });
+  });
+  // describe('AND: An error prevents the photos from loading,', ()=>{
+  //   it('THEN: An error message displays instead of the photos.', ()=>{
+  //     render(
+  //       <Root initialState={initialState}>
+  //         <Display />
+  //       </Root>
+  //     );
+  //     const errorMessage = screen.getByTestId('error-message');
+  //     expect(errorMessage).toBeDefined();
+  //     expect(errorMessage).toHaveTextContent(errorLoadingPhotos.english);
+  //   });
+  //   it('THEN: Localized error messages display properly.', ()=>{
+  //     initialState.language = 'chinese';
+  //     render(
+  //       <Root initialState={initialState}>
+  //         <Display />
+  //       </Root>
+  //     );
+  //     const errorMessage = screen.getByTestId('error-message');
+  //     expect(errorMessage).toHaveTextContent(errorLoadingPhotos.chinese);
+  //   });
+  // });
 });
