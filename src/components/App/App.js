881edf85bf85cf366ffe999ec2d898011285ac36
Alex Gochenour <alexgochenour@gmail.com>
2021-06-24 16:14:28 -0400

    WIP: Stepping out for a minute.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,13 +17,19 @@ export default function App() {
     dispatchTempAndHumidity(dispatch);
   });
   const dispatch = useDispatch();
-    return (
-      <div id='app' data-test="component-app">
+  return (
+    <div id='app' data-test="component-app">
+      <div id='heading-wrapper'>
         <Heading/>
+      </div>
+      <div id=''>
         <FotoDisplay />
+      </div>
+      <div id=''>
         <Copyright/>
       </div>
-    );
+    </div>
+  );
 }
 
 const dispatchTempAndHumidity = (dispatcher) => {
