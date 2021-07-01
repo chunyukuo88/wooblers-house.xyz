e7949710fefb1d26ecf8bee5ff434b1efc5ac5e0
Alex Gochenour <alexgochenour@gmail.com>
2021-07-01 18:50:33 -0400

    Tests for LocalizedTitle.

--- src/components/Title/LocalizedTitle.js
+++ src/components/Title/LocalizedTitle.js
@@ -3,24 +3,18 @@ import Hedgehog from '../Hedgehog/Hedgehog.jsx';
 
 export const LocalizedTitle = (props) => {
   const { language, localizedString } = props;
-  const className = _createClassName(language);
 
   const hedgehogProps = { language: language };
 
-    return (
-      <div className='title-container'>
-        <div data-testid='title-string'
-             className={className}>
-          {localizedString}
-        </div>
-        <div>
-          <RussianHedgehog {...hedgehogProps}/>
-        </div>
+  return (
+    <div className='title-container'>
+      <div data-testid='title-string'
+           className={`${language}-title`}>
+        {localizedString}
       </div>
-    );
+      <RussianHedgehog {...hedgehogProps}/>
+    </div>
+  );
 };
 
 const RussianHedgehog = ({ language }) => (language === 'russian') ? <Hedgehog/> : null;
-
-const _createClassName = (language) => `${language}-title`;
-
--- src/components/Title/LocalizedTitle.test.js
+++ src/components/Title/LocalizedTitle.test.js
@@ -6,16 +6,37 @@ describe('LocalizedTitle.js', ()=>{
   describe('LocalizedTitle()', ()=>{
     describe('GIVEN: Props containing a language and a localized string,', ()=>{
       it('THEN: The component renders the title string properly.', ()=>{
-        const testProps = {
+        const props = {
           language: 'chinese',
           localizedString: '测试测试',
         };
         render(
-          <LocalizedTitle props={testProps}/>
+          <LocalizedTitle {...props}/>
         );
         const localizedTitleString = screen.getByTestId('title-string');
-
-        expect(localizedTitleString).toHaveTextContent(testProps.localizedString);
+        expect(localizedTitleString).toHaveTextContent(props.localizedString);
+      });
+      it('THEN: And it works in English.', ()=>{
+        const props = {
+          language: 'english',
+          localizedString: 'test test',
+        };
+        render(
+          <LocalizedTitle {...props}/>
+        );
+        const localizedTitleString = screen.getByTestId('title-string');
+        expect(localizedTitleString).toHaveTextContent(props.localizedString);
+      });
+      it('THEN: And it works in any other language.', ()=>{
+        const props = {
+          language: 'gibberish',
+          localizedString: 'asdfasdfasdf',
+        };
+        render(
+          <LocalizedTitle {...props}/>
+        );
+        const localizedTitleString = screen.getByTestId('title-string');
+        expect(localizedTitleString).toHaveTextContent(props.localizedString);
       });
     });
   });
