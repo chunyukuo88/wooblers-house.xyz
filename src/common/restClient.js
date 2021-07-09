5e075a3574cb997e23dd06ef3d7c9265a56ec047
Alex Gochenour <alexgochenour@gmail.com>
2021-07-08 23:09:21 -0400

    console logs nixed

--- src/common/restClient.js
+++ src/common/restClient.js
@@ -1,20 +1,17 @@
 import urls from '../urls';
 
-const { log, error } = console;
-
 export const fetchJsonData = async (urlString) => {
   return await fetch(urlString)
     .then(res => res.json()
-    .catch(err => error(err)));
+    .catch(err => console.error(err)));
 };
 
 export const getWeatherDatum = async (weatherAttribute) => {
   try {
     const data = await fetchJsonData(urls.openWeatherUrl);
-    log(data);
     return getSpecificDataPoint(data, weatherAttribute);
   } catch (e) {
-    error('fetchWeather() failed:', e.message);
+    console.error('fetchWeather() failed:', e.message);
   }
 };
 
