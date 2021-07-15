2a5694dd95189ea0d997ab8995584ace01769aba
Alex Gochenour <alexgochenour@gmail.com>
2021-07-15 11:10:13 -0400

    Made a second click event. Next: Refactor such that GA category and action strings are all stored in one convenient place and imported where needed.

--- src/components/FAQ/utils.js
+++ src/components/FAQ/utils.js
@@ -1,6 +1,11 @@
 import { goToPage } from '../../actionCreators/navActionCreators';
 import { routes } from '../../routes';
+import { fireGoogleAnalyticsEvent } from '../../common/reactGa';
+
+const categoryStr = 'Click - Navigation';
+const actionStr = 'User navigated to main page from FAQ page.';
 
 export const backButtonHandler = (dispatch, history) => {
+  fireGoogleAnalyticsEvent(categoryStr, actionStr);
   dispatch(goToPage(routes.index, history));
 };
--- src/components/FAQ/utils.test.js
+++ src/components/FAQ/utils.test.js
@@ -1,21 +1,37 @@
 import { backButtonHandler } from './utils';
 import { goToPage } from '../../actionCreators/navActionCreators';
 import { routes } from '../../routes';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Root from '../../Root';
+import { Faq } from './Faq';
+import { fireGoogleAnalyticsEvent } from '../../common/reactGa';
 
 jest.mock('../../actionCreators/navActionCreators');
+jest.mock('../../common/reactGa');
 
 describe('utils.js', ()=>{
   describe('backButtonHandler()', ()=>{
+    const dispatch = jest.fn();
+    const history = [];
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
     describe('WHEN: given a dispatch function and a history array,', ()=>{
       it('THEN: The function dispatches an the goToPage action creator with the correct route and the history array.', ()=>{
         goToPage.mockImplementation(jest.fn());
 
-        const dispatch = jest.fn();
-        const history = [];
-
         backButtonHandler(dispatch, history);
 
         expect(goToPage).toHaveBeenCalledWith(routes.index, history);
+      });
+      it('THEN: The event is dispatched to Google Analytics via fireGoogleAnalyticsEvent().', ()=>{
+        fireGoogleAnalyticsEvent.mockImplementation(jest.fn());
+        const categoryStr = 'Click - Navigation';
+        const actionStr = 'User navigated to main page from FAQ page.';
+
+        backButtonHandler(dispatch, history);
+
+        expect(fireGoogleAnalyticsEvent).toHaveBeenCalledWith(categoryStr, actionStr);
       });
     });
   });
