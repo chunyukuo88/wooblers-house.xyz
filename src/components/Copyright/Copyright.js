36180ef45d166a761ef674e35bd0609584d3c400
Alex Gochenour <alexgochenour@gmail.com>
2021-07-15 18:54:51 -0400

    Finished routing for login page.

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { copyrightStringHandler } from './utils';
 import staticStrings from '../../StaticStrings';
 import '../../css/Copyright.css';
+import { useHistory } from 'react-router-dom';
 
 export default function Copyright(){
+  const dispatch = useDispatch();
+  const history = useHistory();
   const language = useSelector((state) => state.language);
   const copyrightString = staticStrings.copyright[language];
   const currentYear = new Date().getFullYear();
@@ -11,8 +15,8 @@ export default function Copyright(){
   return (
     <div id='copyright'
          className={`${language} copyright`}
-    >
-      <span>{copyrightString}</span>
+         onClick={()=>copyrightStringHandler(dispatch, history)}>
+      <span data-testid='copyright-string'>{copyrightString}</span>
       <span data-testid='current-year'>{currentYear}</span>
       <span data-testid='chinese-char-for-year'>{年}</span>
     </div>
