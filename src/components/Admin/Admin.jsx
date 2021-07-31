fd0f7acf5136b0b06f5a29e1ded109e131160a11
Alex Gochenour <alexgochenour@gmail.com>
2021-07-31 16:24:08 -0400

    Updated configuration format to no longer import aws-amplify.js but instead configure in the Admin.jsx module, as specified in the Amplify Storage documentation.

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Amplify from 'aws-amplify';
-import awsconfig from '../../aws-exports';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '../../css/Admin.css';
 import { ProtectedContent } from './ProtectedContent.jsx';
 
-Amplify.configure(awsconfig);
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'us-east-1:4fd65d6b-3898-4129-9712-de687ad9aa8b',
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_uEeZqOq9b',
+    userPoolWebClientId: '5fagst079p2vrhu18j0vpieedl',
+  },
+  Storage: {
+    AWSS3: {
+      bucket: 'woobler-photos',
+      region: 'us-east-1',
+    }
+  }
+});
 
 export function Admin() {
   return <div data-testid='protectedContent'>
