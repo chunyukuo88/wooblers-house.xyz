c0bf0e143a58aaf9ae67a5936fad2902c6625b71
Alex Gochenour <alexgochenour@gmail.com>
2021-07-31 23:24:57 -0400

    The code should be fine, but I am getting 403 errors so I suspect there is a problem with AWS configuration.

--- src/components/Admin/ProtectedContent.jsx
+++ src/components/Admin/ProtectedContent.jsx
@@ -2,36 +2,40 @@ import React from 'react';
 import { AmplifySignOut } from '@aws-amplify/ui-react';
 import { amplifyConfig } from '../../config';
 import { uploadPhotoToS3 } from './utils/uploadToS3';
-// import {
-//   ListGroup,
-//   FormGroup,
-//   FormControl,
-// } from 'react-bootstrap';
 
 /**
 Xiao3wu1zhi1wu1
  * */
 export const ProtectedContent = () => {
   const [ selectedFile, setSelectedFile ] = React.useState(null);
+
+  const fileSelectionHandler = (event) => setSelectedFile(event.target.files[0]);
+
+  const fileUploadHandler = async (selectedFile) => {
+    const fd = createFormData(selectedFile);
+    await handleSubmit(fd);
+  };
+
   return (
     <>
       <AmplifySignOut/>
-      <h1 className='english'>You are logged in</h1>
-      <input type='file' onChange={fileSelectionHandler}/>
-      <button onClick={fileUploadHandler}>Upload image</button>
+      <section className='logged-in-section english'>
+        <h1 >You are logged in</h1>
+        <input type='file'
+               onChange={(e) => fileSelectionHandler(e)}
+        />
+        <button onClick={() => fileUploadHandler(selectedFile)}>
+          Upload image
+        </button>
+      </section>
     </>
   );
 }
 
-const fileSelectionHandler = (setSelectedFile, event) => {
-  event.preventDefault();
-  setSelectedFile(event.target.files[0]);
-};
-
-const fileUploadHandler = (selectedFile) => {
+const createFormData = (selectedFile) => {
   const fd = new FormData();
   fd.append('image', selectedFile, selectedFile.name);
-  handleSubmit(fd);
+  return fd;
 };
 
 export const handleSubmit = async (file) => {
