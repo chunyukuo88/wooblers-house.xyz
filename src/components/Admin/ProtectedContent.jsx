addb530f3822143fbf8005b66df8df8a7c10554f
Alex Gochenour <alexgochenour@gmail.com>
2021-08-11 13:38:03 -0400

    Fixed test for excessively sized files in submissionHandler().

--- src/components/Admin/ProtectedContent.jsx
+++ src/components/Admin/ProtectedContent.jsx
@@ -3,27 +3,24 @@ import { AmplifySignOut } from '@aws-amplify/ui-react';
 import { amplifyConfig } from '../../config';
 import { uploadPhotoToS3 } from './utils/uploadToS3';
 
-/**
-Xiao3wu1zhi1wu1
- * */
 export const ProtectedContent = () => {
   const [ selectedFile, setSelectedFile ] = React.useState(null);
 
-  const fileSelectionHandler = (event) => setSelectedFile(event.target.files[0]);
+  const fileSelectionHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+  }
 
   const fileUploadHandler = async (selectedFile) => {
     const fd = createFormData(selectedFile);
-    await handleSubmit(fd);
+    await submissionHandler(fd);
   };
 
   return (
     <>
       <AmplifySignOut/>
-      <section className='logged-in-section english'>
+      <section data-testid='logged-in-section' className='logged-in-section english'>
         <h1 >You are logged in</h1>
-        <input type='file'
-               onChange={(e) => fileSelectionHandler(e)}
-        />
+        <input type='file' onChange={(e) => fileSelectionHandler(e)}/>
         <button onClick={() => fileUploadHandler(selectedFile)}>
           Upload image
         </button>
@@ -38,15 +35,25 @@ const createFormData = (selectedFile) => {
   return fd;
 };
 
-export const handleSubmit = async (file) => {
-  if (fileIsTooBig(file)) { fileIsTooBigAlert(); return; }
-  try {
-    await uploadPhotoToS3(file);
-  } catch (e) {
-    alert(e);
+export const submissionHandler = async (file) => {
+  if(isNotJpeg(file)) {
+    alert(invalidFileMsg);
+    return;
+  };
+  if (fileIsTooBig(file)) {
+    fileIsTooBigAlert(); return;
   }
+  try {
+    await uploadPhotoToS3(file); }
+  catch (e) { alert(e); }
 }
 
-const fileIsTooBigAlert = () => alert(`Please pick a file smaller than ${amplifyConfig.MAX_ATTACHMENT_SIZE/1_000_000} MB.`);
+const invalidFileMsg = 'Please select a valid JPG file.';
+
+const fileIsTooBigAlert = () => alert(
+  `Please pick a file smaller than ${amplifyConfig.MAX_ATTACHMENT_SIZE/1_000_000} MB.`
+);
 
 const fileIsTooBig = (file) => file && file.size > amplifyConfig.MAX_ATTACHMENT_SIZE;
+
+const isNotJpeg = (file) => file.type !== 'image/jpeg';
--- src/components/Admin/tests/ProtectedContent.test.js
+++ src/components/Admin/tests/ProtectedContent.test.js
@@ -1,10 +1,58 @@
-import { render } from '@testing-library/react';
-import { ProtectedContent } from '../ProtectedContent.jsx';
+import { render, screen } from '@testing-library/react';
+import { ProtectedContent, submissionHandler } from '../ProtectedContent.jsx';
+import { uploadPhotoToS3 } from '../utils/uploadToS3';
+import { amplifyConfig } from '../../../config';
 
-it('The content loads properly.', ()=>{
-  const { container } = render(
-    <ProtectedContent/>
-  );
+jest.mock('../utils/uploadToS3');
 
-  expect(container).toBeInTheDocument();
+describe('ProtectedContent.jsx', ()=>{
+  describe('ProtectedContent()', ()=>{
+    it('The content loads properly.', ()=>{
+      render(<ProtectedContent/>);
+      const loggedInSection = screen.getByTestId('logged-in-section');
+
+      expect(loggedInSection).toBeInTheDocument();
+    });
+  });
+  describe('submissionHandler()', ()=>{
+    let file = {};
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+    describe('WHEN: Given a file that is too big', ()=>{
+      it('THEN: The function alerts the user and returns nothing.', async ()=>{
+        const spy = jest.spyOn(window, 'alert').mockImplementation(() => {});
+        const errorMsg = `Please pick a file smaller than ${amplifyConfig.MAX_ATTACHMENT_SIZE/1_000_000} MB.`;
+        file.size = 5_000_001;
+        file.type = 'image/jpeg';
+
+        const result = await submissionHandler(file);
+
+        expect(spy).toHaveBeenCalledWith(errorMsg);
+        expect(result).toBeUndefined();
+      });
+    });
+    describe('WHEN: Given a valid file', ()=>{
+      it('THEN: The function uploads to S3 and returns nothing.', async ()=>{
+        uploadPhotoToS3.mockImplementation(jest.fn());
+        file.size = 1_000;
+
+        await submissionHandler(file);
+
+        expect(uploadPhotoToS3).toHaveBeenCalledWith(file);
+      });
+    });
+    describe('WHEN: Given a file that is within the proper size but is not an image', ()=>{
+      it('THEN: The function present the error with an alert.', async ()=>{
+        const spy = jest.spyOn(window, 'alert').mockImplementation(() => {});
+        const invalidFileMsg = 'Please select a valid JPG file.'
+        file = [];
+
+        await submissionHandler(file);
+
+        expect(spy).toHaveBeenCalledWith(invalidFileMsg);
+        expect(uploadPhotoToS3).not.toHaveBeenCalledWith(file);
+      });
+    });
+  });
 });
