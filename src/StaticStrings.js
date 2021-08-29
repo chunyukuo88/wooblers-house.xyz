4cd5fb569743fc34306042f853ceae0a53201cf1
chunyukuo88 <alexgochenour@gmail.com>
2021-08-29 00:42:35 -0400

    More CSS improvements.

--- src/StaticStrings.js
+++ src/StaticStrings.js
@@ -1,84 +1,90 @@
 const staticStrings = {
   code: {
-    english: 'Github',
-    russian: 'Код',
-    chinese: '代碼'
+    english: "Github",
+    russian: "Код",
+    chinese: "代碼",
   },
   copyright: {
-    english: 'Copyright Alex Gochenour ',
-    russian: 'Авторские права Алекс Гохинауэр ',
-    chinese: '©Alex Gochenour集团 '
+    english: "Copyright Alex Gochenour ",
+    russian: "Авторские права Алекс Гохинауэр ",
+    chinese: "©Alex Gochenour集团 ",
   },
   errorLoadingPhotos: {
-    english: 'There was an error loading the photos. Please contact the webmaster.',
-    russian: 'При загрузке фотографий произошла ошибка. Обратитесь к веб-мастеру.',
-    chinese: '下載圖片失敗，請聯繫郭磊'
+    english:
+      "There was an error loading the photos. Please contact the webmaster.",
+    russian:
+      "При загрузке фотографий произошла ошибка. Обратитесь к веб-мастеру.",
+    chinese: "下載圖片失敗，請聯繫郭磊",
   },
   faqButton: {
-    english: 'FAQ',
-    russian: 'ЧАВО',
-    chinese: '常見問題',
+    english: "FAQ",
+    russian: "ЧАВО",
+    chinese: "常見問題",
   },
   faqComingSoon: {
-    english: 'This page is coming soon!',
-    russian: 'Страница часто задаваемых вопросов скоро появится!',
-    chinese: '常見問題页八月出爐！',
+    english: "This page is coming soon!",
+    russian: "Страница часто задаваемых вопросов скоро появится!",
+    chinese: "常見問題页八月出爐！",
   },
   faqRouteToMain: {
-    english: 'Back to main',
-    russian: 'Вернуться на главную',
-    chinese: '主頁',
+    english: "Home",
+    russian: "Вернуться на главную",
+    chinese: "主頁",
   },
   languageLabel: {
-    english: 'English',
-    russian: 'русский',
-    chinese: '正體中文'
+    english: "English",
+    russian: "русский",
+    chinese: "正體中文",
   },
   loadingMessage: {
-    english: 'Loading...',
-    russian: 'загрузка ...',
-    chinese: '加載中，請稍候'
+    english: "Loading...",
+    russian: "загрузка ...",
+    chinese: "加載中，請稍候",
   },
   modalCoverage: {
-    english: 'To achieve 100% test coverage, I used Enzyme at first but later switched to React Testing Library.',
-    russian: '100% тестового покрытия с библиотекой Enzyme и React Testing Library.',
-    chinese: '為了達成百分百測試覆蓋率，我用了Enzyme 與 React Testing Library。'
+    english:
+      "To achieve 100% test coverage, I used Enzyme at first but later switched to React Testing Library.",
+    russian:
+      "100% тестового покрытия с библиотекой Enzyme и React Testing Library.",
+    chinese:
+      "為了達成百分百測試覆蓋率，我用了Enzyme 與 React Testing Library。",
   },
   modalHeading: {
-    english: '...to my trilingual photo album.',
-    russian: '... в мой трехъязычный фотоальбом.',
-    chinese: '歡迎訪問小巫之屋！'
+    english: "...to my trilingual photo album.",
+    russian: "... в мой трехъязычный фотоальбом.",
+    chinese: "歡迎訪問小巫之屋！",
   },
   modalSummary: {
-    english: 'This is a test-driven React/Redux project. Try it out and let me know what you think!',
-    russian: 'Это проект React/Redux. Попробуйте!',
-    chinese: '製作本站，我用了REACT與REDUX。'
+    english:
+      "This is a test-driven React/Redux project. Try it out and let me know what you think!",
+    russian: "Это проект React/Redux. Попробуйте!",
+    chinese: "製作本站，我用了REACT與REDUX。",
   },
   titleStrings: {
-    english: 'Woobler\'s House',
-    russian: 'Берлога Пуха',
-    chinese: '小巫之屋',
+    english: "Woobler's House",
+    russian: "Берлога Пуха",
+    chinese: "小巫之屋",
   },
   weatherHumidity: {
-    english: 'Humidity: ',
-    russian: 'Влажность: ',
-    chinese: '濕度：'
+    english: "Humidity: ",
+    russian: "Влажность: ",
+    chinese: "濕度：",
   },
   weatherLabels: {
-    english: 'W\'ville Weather',
-    russian: 'Вестервиль Погода',
-    chinese: '韋斯特維爾天氣'
+    english: "W'ville Weather",
+    russian: "Вестервиль Погода",
+    chinese: "韋斯特維爾天氣",
   },
   weatherTemp: {
-    english: 'Temperature: ',
-    russian: 'Температура: ',
-    chinese: '溫度：'
+    english: "Temperature: ",
+    russian: "Температура: ",
+    chinese: "溫度：",
   },
   welcome: {
-    english: 'Welcome',
-    russian: 'пожаловать',
-    chinese: '簡介'
+    english: "Welcome",
+    russian: "пожаловать",
+    chinese: "簡介",
   },
-}
+};
 
 export default staticStrings;
