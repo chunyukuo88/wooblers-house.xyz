8891106f24386e5a8d40fb98c0e458d8ddbdeff6
chunyukuo88 <alexgochenour@gmail.com>
2021-09-13 20:58:22 -0400

    Fixed the broken left arrow.

--- src/components/Carousel/Display.js
+++ src/components/Carousel/Display.js
@@ -1,77 +1,89 @@
-import React from 'react';
-import '../../css/Display.css';
-import staticStrings from '../../StaticStrings';
-import { useSelector } from 'react-redux';
-import { ReactComponent as HandDrawnArrow } from './arrow_button.svg';
+import React from "react";
+import "../../css/Display.css";
+import staticStrings from "../../StaticStrings";
+import { useSelector } from "react-redux";
+import { ReactComponent as HandDrawnArrow } from "./arrow_button.svg";
 
 export const Display = ({ photosObject }) => {
   const [currentPhotoNumber, setCurrent] = React.useState(0);
-  const isLeft = false;
+  const pointsLeft = false;
   return (
-    <div id='display-wrapper'>
+    <div id="display-wrapper">
       <Arrow {...{ currentPhotoNumber, setCurrent, photosObject }} />
       <Content {...{ photosObject, currentPhotoNumber }} />
-      <Arrow {...{ currentPhotoNumber, setCurrent, photosObject, isLeft }} />
+      <Arrow
+        {...{ currentPhotoNumber, setCurrent, photosObject, pointsLeft }}
+      />
     </div>
   );
 };
 
 const Content = ({ photosObject, currentPhotoNumber }) =>
-  numberOfPhotos(photosObject) === 0
-    ? (<NoPhotosMessage />)
-    : (<div className='displayed-photo'><DisplayedPhoto {...{photosObject, currentPhotoNumber}} /></div>);
+  numberOfPhotos(photosObject) === 0 ? (
+    <NoPhotosMessage />
+  ) : (
+    <div className="displayed-photo">
+      <DisplayedPhoto {...{ photosObject, currentPhotoNumber }} />
+    </div>
+  );
 
 const prevPhoto = (currentPhotoNumber, setCurrent, photosObject) => {
   if (currentPhotoNumber === 0) {
     setCurrent(numberOfPhotos(photosObject) - 1);
-  }
-  else setCurrent(currentPhotoNumber - 1);
+  } else setCurrent(currentPhotoNumber - 1);
 };
 
 const nextPhoto = (currentPhotoNumber, setCurrent, photosObject) => {
   if (currentPhotoNumber === numberOfPhotos(photosObject) - 1) {
     setCurrent(0);
-  }
-  else setCurrent(currentPhotoNumber + 1);
+  } else setCurrent(currentPhotoNumber + 1);
 };
 
-const Arrow = ({ currentPhotoNumber, setCurrent, photosObject, isLeft = true }) => {
-  const direction = (isLeft) ? 'left' : 'right';
-  const clickHandler = (isLeft) ? prevPhoto : nextPhoto;
+const Arrow = ({
+  currentPhotoNumber,
+  setCurrent,
+  photosObject,
+  pointsLeft = true,
+}) => {
+  const direction = pointsLeft ? "left" : "right";
+  const clickHandler = pointsLeft ? prevPhoto : nextPhoto;
   return (
     numberOfPhotos(photosObject) > 0 && (
-      <div className='arrow-wrapper' id={`${direction}-arrow-wrapper`}>
+      <div className="arrow-wrapper" id={`${direction}-arrow-wrapper`}>
         <HandDrawnArrow
           data-testid={`${direction}-arrow`}
           id={`${direction}-arrow`}
-          onClick={() => clickHandler(currentPhotoNumber, setCurrent, photosObject)}
+          onClick={() =>
+            clickHandler(currentPhotoNumber, setCurrent, photosObject)
+          }
         />
       </div>
-  ));
+    )
+  );
 };
 
 const NoPhotosMessage = () => {
-  const language = useSelector(({language}) => language);
+  const language = useSelector(({ language }) => language);
   return (
     <div
       className={language}
-      data-testid='no-photos-message'
-      id='no-photos-message'
+      data-testid="no-photos-message"
+      id="no-photos-message"
     >
       {staticStrings.loadingMessage[language]}
     </div>
   );
 };
 
-const DisplayedPhoto = ({photosObject, currentPhotoNumber}) => {
+const DisplayedPhoto = ({ photosObject, currentPhotoNumber }) => {
   const photoUrl = getPhotoUrl(photosObject, currentPhotoNumber);
   return (
     <img
       className={photoUrl}
-      data-testid='photo'
+      data-testid="photo"
       key={currentPhotoNumber}
       src={photoUrl}
-      alt='test'
+      alt="test"
     />
   );
 };
@@ -79,6 +91,6 @@ const DisplayedPhoto = ({photosObject, currentPhotoNumber}) => {
 const numberOfPhotos = (photosObject) => photosObject?.Contents.length || 0;
 
 const getPhotoUrl = (photosObject, currentPhotoNumber) => {
-  const key = photosObject.Contents[currentPhotoNumber]?.['Key'];
+  const key = photosObject.Contents[currentPhotoNumber]?.["Key"];
   return `${process.env.REACT_APP_FOTO_SOURCE}/${key}`;
-}
+};
--- src/components/Carousel/tests/Display.test.js
+++ src/components/Carousel/tests/Display.test.js
@@ -1,7 +1,7 @@
-import { Display } from '../Display';
-import { fireEvent, render, screen } from '@testing-library/react';
-import staticStrings from '../../../StaticStrings';
-import Root from '../../../Root';
+import { Display } from "../Display";
+import { fireEvent, render, screen } from "@testing-library/react";
+import staticStrings from "../../../StaticStrings";
+import Root from "../../../Root";
 
 const props = {
   photosObject: {
@@ -9,102 +9,102 @@ const props = {
   },
 };
 
-const initialState = { language: 'english' };
+const initialState = { language: "english" };
 
-describe('Display()', () => {
+describe("Display()", () => {
   beforeEach(() => {
     props.photosObject.Contents = [
-      { Key: '0' },
-      { Key: '1' },
-      { Key: '2' },
-      { Key: '3' },
+      { Key: "0" },
+      { Key: "1" },
+      { Key: "2" },
+      { Key: "3" },
     ];
   });
-  describe('GIVEN: The component has loaded,', () => {
-    it('THEN: The first photograph is displayed.', () => {
+  describe("GIVEN: The component has loaded,", () => {
+    it("THEN: The first photograph is displayed.", () => {
       render(<Display {...props} />);
-      const displayedImage = screen.getByTestId('photo');
+      const displayedImage = screen.getByTestId("photo");
 
-      expect(displayedImage).toHaveClass('www.mock-photo-source.com/0');
+      expect(displayedImage).toHaveClass("www.mock-photo-source.com/0");
     });
   });
-  describe('WHEN: The user clicks the right arrow', () => {
-    it('THEN: The next photograph is revealed.', () => {
+  describe("WHEN: The user clicks the right arrow", () => {
+    it("THEN: The next photograph is revealed.", () => {
       render(<Display {...props} />);
-      const rightArrow = screen.getByTestId('right-arrow');
+      const rightArrow = screen.getByTestId("right-arrow");
 
       fireEvent.click(rightArrow);
-      const displayedImage = screen.getByTestId('photo');
+      const displayedImage = screen.getByTestId("photo");
 
-      expect(displayedImage).toHaveClass('www.mock-photo-source.com/1');
+      expect(displayedImage).toHaveClass("www.mock-photo-source.com/1");
     });
   });
-  describe('WHEN: The user clicks the left arrow after viewing the second photo,', () => {
-    it('THEN: The first photo is revealed.', () => {
+  describe("WHEN: The user clicks the left arrow after viewing the second photo,", () => {
+    it("THEN: The first photo is revealed.", () => {
       render(<Display {...props} />);
-      const rightArrow = screen.getByTestId('right-arrow');
-      const leftArrow = screen.getByTestId('left-arrow');
+      const rightArrow = screen.getByTestId("right-arrow");
+      const leftArrow = screen.getByTestId("left-arrow");
 
-      let displayedImage = screen.getByTestId('photo');
+      let displayedImage = screen.getByTestId("photo");
 
-      expect(displayedImage).toHaveClass('www.mock-photo-source.com/0');
+      expect(displayedImage).toHaveClass("www.mock-photo-source.com/0");
 
       fireEvent.click(rightArrow);
-      displayedImage = screen.getByTestId('photo');
+      displayedImage = screen.getByTestId("photo");
 
-      expect(displayedImage).toHaveClass('www.mock-photo-source.com/1');
+      expect(displayedImage).toHaveClass("www.mock-photo-source.com/1");
 
       fireEvent.click(leftArrow);
-      displayedImage = screen.getByTestId('photo');
+      displayedImage = screen.getByTestId("photo");
 
-      expect(displayedImage).toHaveClass('www.mock-photo-source.com/0');
+      expect(displayedImage).toHaveClass("www.mock-photo-source.com/0");
     });
   });
-  describe('WHEN: The user clicks the left arrow while viewing the first photo', () => {
-    it('THEN: It cycles back to the last photo.', () => {
+  describe("WHEN: The user clicks the left arrow while viewing the first photo", () => {
+    it("THEN: It cycles back to the last photo.", () => {
       render(<Display {...props} />);
-      const leftArrow = screen.getByTestId('left-arrow');
+      const leftArrow = screen.getByTestId("left-arrow");
 
       fireEvent.click(leftArrow);
-      const displayedImage = screen.getByTestId('photo');
+      const displayedImage = screen.getByTestId("photo");
 
-      expect(displayedImage).toHaveClass('www.mock-photo-source.com/3');
+      expect(displayedImage).toHaveClass("www.mock-photo-source.com/3");
     });
   });
-  describe('WHEN: The user clicks the right arrow and there are no more photos', () => {
-    it('THEN: It cycles back to the first photo.', () => {
+  describe("WHEN: The user clicks the right arrow and there are no more photos", () => {
+    it("THEN: It cycles back to the first photo.", () => {
       render(<Display {...props} />);
-      const rightArrow = screen.getByTestId('right-arrow');
+      const rightArrow = screen.getByTestId("right-arrow");
 
       fireEvent.click(rightArrow);
       fireEvent.click(rightArrow);
       fireEvent.click(rightArrow);
-      let displayedImage = screen.getByTestId('photo');
+      let displayedImage = screen.getByTestId("photo");
 
-      expect(displayedImage).toHaveClass('www.mock-photo-source.com/3');
+      expect(displayedImage).toHaveClass("www.mock-photo-source.com/3");
 
       fireEvent.click(rightArrow);
-      displayedImage = screen.getByTestId('photo');
+      displayedImage = screen.getByTestId("photo");
 
-      expect(displayedImage).toHaveClass('www.mock-photo-source.com/0');
+      expect(displayedImage).toHaveClass("www.mock-photo-source.com/0");
     });
   });
-  describe('WHEN: There are no photos, ', () => {
+  describe("WHEN: There are no photos, ", () => {
     beforeEach(() => {
       props.photosObject.Contents = [];
     });
-    it('THEN: The default photo length is set to zero and a Loading message is shown.', () => {
+    it("THEN: The default photo length is set to zero and a Loading message is shown.", () => {
       render(
         <Root initialState={initialState}>
           <Display {...props} />
         </Root>
       );
-      const noPhotosMessage = screen.getByTestId('no-photos-message');
+      const noPhotosMessage = screen.getByTestId("no-photos-message");
 
       expect(noPhotosMessage).toBeDefined();
     });
-    it('AND: the Loading message is localized.', () => {
-      initialState.language = 'russian';
+    it("AND: the Loading message is localized.", () => {
+      initialState.language = "russian";
 
       const expectedMessage = staticStrings.loadingMessage.russian;
       render(
@@ -112,19 +112,19 @@ describe('Display()', () => {
           <Display {...props} />
         </Root>
       );
-      const noPhotosMessage = screen.getByTestId('no-photos-message');
+      const noPhotosMessage = screen.getByTestId("no-photos-message");
 
       expect(noPhotosMessage).toHaveTextContent(expectedMessage);
     });
-    it('AND: The photo display and arrows disappear.', () => {
+    it("AND: The photo display and arrows disappear.", () => {
       render(
         <Root initialState={initialState}>
           <Display {...props} />
         </Root>
       );
-      const leftArrow = document.querySelector('#left-arrow');
+      const leftArrow = document.querySelector("#left-arrow");
 
-      const rightArrow = document.querySelector('#right-arrow');
+      const rightArrow = document.querySelector("#right-arrow");
 
       expect(leftArrow).toBeNull();
       expect(rightArrow).toBeNull();
